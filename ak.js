22052fe2d4ffed88d8df37707c1cc99e6e38f20b
Akesh <akeshrao0909a.r@gmail.com>
2021-08-15 19:53:31 +0530

    Initial Commit

---
+++ ak.js
@@ -0,0 +1,22 @@
+function myfun1() {
+  window.location.href = "index.html";
+}
+
+function myfun2() {
+  window.location.href = "aboutus.html";
+}
+function myfun3() {
+  window.location.href = "joinus.html";
+}
+function myfun4() {
+  window.location.href = "contact.html";
+}
+function exercise(n) {
+  if (n == 1) window.location.href = "chest.html";
+  if (n == 2) window.location.href = "back.html";
+  if (n == 3) window.location.href = "arms.html";
+  if (n == 4) window.location.href = "abs.html";
+  if (n == 5) window.location.href = "legs.html";
+  if (n == 6) window.location.href = "shoulders.html";
+  if (n == 7) window.location.href = "diet.html";
+}
